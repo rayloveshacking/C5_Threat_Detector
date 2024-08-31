95a639ca1ba0e128ff502c06d20b1f91acf96271
ggcoder <s10269430@connect.np.edu.sg>
2024-08-31 14:40:01 +0800

    Final Changes

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_application_1/usage/usage.dart';
-import 'package:flutter_application_1/views/login_view.dart';
 import 'firebase_options.dart';
 
 void main() async {
